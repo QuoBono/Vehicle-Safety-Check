6d1dd48cc5f9ab9b7d48ca405c1dd57be572ce9d
Maria-Bianca Cindroi <bianca.cindroi@gmail.com>
2017-03-06 16:14:52 +0100

    Update VehicleSafetyCheck.c

--- VehicleSafetyCheck.c
+++ VehicleSafetyCheck.c
@@ -1,40 +1,37 @@
+/*
+*author Group9
+*/
+
 #include <Servo.h>
+int right = 120, left = 0, straight = 70;
 
-int right = 120; 
-int left = 0;
-int straight = 70;
+int servoPin = 3;
+int feedbackPin = 5;
+int speedPin = 11;
 
-int servoPin = 9;
-int analogPin = A8;
-Servo myServo;
-
+Servo EscServo;
+Servo SteerServo;
 
 void setup(){
   Serial.begin(9600);
-  myServo.attach(servoPin);
-  pinMode(3, INPUT);
-  pinMode(5, INPUT);
-
+  
+  pinMode(servoPin, INPUT);
+  pinMode(feedbackPin, INPUT);
+  
+  pinMode(speedPin, OUTPUT);
+  EscServo.attach(speedPin); 
+  SteerServo.attach(servoPin);
+  
 }
-
 
 void loop(){
 
-  //read the byte values of the pins
   int i = pulseIn(3, HIGH);
   int j = pulseIn(5, HIGH);
+  //Serial.println(steer);
+  SteerServo.write(i);   
   
-  int steer = i/20;
-  Serial.println(steer);
-  //delay(5);
-
-  //initial algorithm for the steering angles 
-  if(steer>63 || steer <75){
-    myServo.write(steer);
-  }else if(steer>75){
-    myServo.write(steer*2);
-  }else if(steer<65){
-    myServo.write(steer/2);
-  }
-    
+  
+  //Serial.println(j);
+  EscServo.writeMicroseconds(j);
 }
