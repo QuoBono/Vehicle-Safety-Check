1ce5b4005c3bb0e45e8d40529bb8f9cf373b52ba
Ninosauleo <ninosauleo@gmail.com>
2017-03-13 12:33:35 +0100

    Clean Code

--- VehicleSafetyCheck.c
+++ VehicleSafetyCheck.c
@@ -1,37 +1,53 @@
 /*
-*author Group9
-*/
+ *author Group9
+ */
 
 #include <Servo.h>
 int right = 120, left = 0, straight = 70;
 
-int servoPin = 3;
-int feedbackPin = 5;
-int speedPin = 11;
+int speedCarInput = 5; //speed
+int steerCarInput = 6; //steering
+int steerCarOutput = 9 ;
+int SpeedCarOutput = 11;
 
 Servo EscServo;
 Servo SteerServo;
 
 void setup(){
-  Serial.begin(9600);
-  
-  pinMode(servoPin, INPUT);
-  pinMode(feedbackPin, INPUT);
-  
-  pinMode(speedPin, OUTPUT);
-  EscServo.attach(speedPin); 
-  SteerServo.attach(servoPin);
-  
+    Serial.begin(9600);
+    
+    pinMode(speedCarInput, INPUT);
+    pinMode(steerCarInput, INPUT); //steering
+    pinMode(steerCarOutput, OUTPUT);
+    pinMode(SpeedCarOutput, OUTPUT);
+    EscServo.attach(11); //11 speed pin
+    //EscServo.attach(feedbackPin);
+    SteerServo.attach(9); //steering
+    
 }
 
 void loop(){
-
-  int i = pulseIn(3, HIGH);
-  int j = pulseIn(5, HIGH);
-  //Serial.println(steer);
-  SteerServo.write(i);   
-  
-  
-  //Serial.println(j);
-  EscServo.writeMicroseconds(j);
+    
+    //SteerServo.write(100);
+    int i = pulseIn(speedCarInput, HIGH); //speed
+    int j = pulseIn(steerCarInput, HIGH); //steering
+    
+    Serial.println("this is speed");
+    Serial.println(i);
+    
+    /*
+     Serial.println("this is steer");
+     Serial.println(j);
+     
+     */
+    // Max over 1800
+    // Min around 900
+    //Neutral 1340 to 1380
+    
+    SteerServo.write(j);
+    //SteerServo.write(30);
+    
+    
+    //EscServo.writeMicroseconds(i);
+    delay(500);
 }
