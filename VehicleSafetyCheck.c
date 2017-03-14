36e55fb8da4d4fc760d230f20e27e2012e782357
Blondiejohan <johan.johansson.swe@gmail.com>
2017-03-14 15:03:20 +0100

    Update VehicleSafetyCheck.c

--- VehicleSafetyCheck.c
+++ VehicleSafetyCheck.c
@@ -1,127 +1,124 @@
-/*
- *author Group9
- *
- */
 
-
-/*
- * Speed Max and Min Values
- */
-// Max over 1850
-// Min around 900
-//Neutral 1340 to 1380 ~= 1350
-
-#define NEUTRAL 1350
-#define FULL 1850
-#define BREAK 850
+/*author Group9
+  */
+ 
+#define NEUTRAL 1400
+#define FULL 1600
+#define BREAK 1000
+#define NEUTRALREVERSE 1200
+#define RIGHT 80
+#define LEFT 60
 
 #include <Servo.h>
-/*
- * Steering Max and Min Values
- */
-//max right 120
-//max left 0
-//straight 70
+ 
+int speedCarInput = 5; //speed
+int steerCarInput = 6; //steering
+int steerCarOutput = 9 ;
+int SpeedCarOutput = 11;
 
-
-/*
- * Car Set-Up
- */
 int straight = 70;
-const int speedCarInput = 5; //speed input
-const int steerCarInput = 6; //steering input
-const int steerCarOutput = 9 ;//steering output
-const int SpeedCarOutput = 11; //speed output
-
-/*
- * Remote Control
- */
 bool joystick = false;
-
-/*
- * Serial Control
- */
 char userInput;
-char lastPressed;
-int neutral = NEUTRAL;
-int right = straight + 10;
-int left = straight - 10;
-
-/*
- * Speed and Steer Servo
- */
-Servo EscServo; //Speed
-Servo SteerServo; //Steer
-
+int speedValue = NEUTRAL;
+int right = RIGHT;
+int left = LEFT;
+int speedValueRev = NEUTRALREVERSE;
+bool controller = false;
+int steerValue = straight;
+ 
+ Servo EscServo;
+ Servo SteerServo;
 void setup(){
+ 
     Serial.begin(9600);
-    pinMode(speedCarInput, INPUT);
-    pinMode(steerCarInput, INPUT); //steering
-    pinMode(steerCarOutput, OUTPUT);
-    pinMode(SpeedCarOutput, OUTPUT);
-    EscServo.attach(11); //speed pin
-    SteerServo.attach(9); //steer pin
-    /*
-     * Set car wheels to the front and NEUTRAL speed.
-     */
+    EscServo.attach(11); //11 speed pin
+    SteerServo.attach(9); //steering
     SteerServo.write(straight);
-    EscServo.write(NEUTRAL);
-    
-}
-
-void loop(){
-    /*
-     * If the car receives a signal from the Remote control (either speed or steer) Joystick will be true else false.
-     */
+    EscServo.writeMicroseconds(NEUTRAL);
+ }
+ 
+ void loop(){
+     
+    int i = pulseIn(speedCarInput, HIGH); //speed
+    int j = pulseIn(steerCarInput, HIGH); //steering
+   
     if(pulseIn(speedCarInput, HIGH, 27000) > 800 && pulseIn(steerCarInput, HIGH, 27000) > 800){
         joystick = true;
     }else{
         joystick = false;
-    }
-    
-    
+   }
+   
     if(joystick) {
-        int i = pulseIn(speedCarInput, HIGH); //Get speed
-        int j = pulseIn(steerCarInput, HIGH); //Get steering
+       Serial.println("joystick");  
+     
+
+      if(!controller){
         
-        Serial.println("this is speed");
-        Serial.println(i);
-        Serial.println("this is steer");
-        Serial.println(j);
-        
-        EscServo.writeMicroseconds(i); //speed
-        SteerServo.write(j); //steering
-        
-    } else if (!joystick || Serial.available() > 0) {
-        
-        /*
-         * Get Input from Serial
-         */
-        userInput = Serial.read();
-        
-        /*
-         * Switch case for moving the car
-         */
-        
-        switch(userInput){
-                /*
-                 * Last key pressed is used in order to ovoid damaging the motors when someone goes from front to backwards or the other way.
-                 */
-            case 'w': if(lastPressed == 'x'){ EscServo.write(NEUTRAL); delay(1000);}EscServo.write(neutral + 100); if(neutral < 1550){neutral = neutral + 10;} lastPressed = 'w';
-                break;
-            case 's': EscServo.write(NEUTRAL); SteerServo.write(straight); lastPressed = 's';
-                break;
-            case 'd': SteerServo.write(right); if(right <= 120){ right = right +10;} lastPressed = 'd'; //increment the steering if you press more.
-                break;
-            case 'a': SteerServo.write(left); if(left > 0){ left = left - 10;} lastPressed = 'a'; //increment the steering if you press more.
-                break;
-            case 'x': if(lastPressed == 'w'){ EscServo.write(NEUTRAL); delay(1000);} EscServo.write(NEUTRAL - 200); SteerServo.write(straight); lastPressed = 'w';
-                break;
-        }
-        
-    }
-    delay(50);
+        SteerServo.write(straight);
+        EscServo.writeMicroseconds(NEUTRAL);
+        controller = true;
+        delay(20);
+      }
     
+        int accelerate = pulseIn(speedCarInput, HIGH); //speed
+        int steer = pulseIn(steerCarInput, HIGH); //steering
+        
+        SteerServo.write(steer);
+        EscServo.writeMicroseconds(accelerate);
+
+    } else if (!joystick) {
+      controller = false;
+      userInput = Serial.read();
+
+        switch(userInput){
+            case 'w': 
+                  speedValue = speedValue+50;
+                  break;
+                  
+            case 's': 
+                  speedValue = NEUTRAL;
+                  steerValue = straight;
+                  right = RIGHT;
+                  left = LEFT;
+                  speedValueRev = NEUTRALREVERSE;
+                  break;
+                  
+            case 'd':
+                  right = right + 10;
+                  steerValue = right;
+                  break;
+                  
+            case 'a':  
+                  left  = left - 10;
+                  steerValue = left;
+                  break;
+                  
+            case 'x': 
+                  EscServo.writeMicroseconds(NEUTRAL);
+                  delay(100);
+                  speedValueRev = speedValueRev - 50;
+                  speedValue = speedValueRev;
+                  break;
+        }
+    }
+    
+    if (speedValue > FULL) {
+        speedValue = FULL;
+    }
+    if (speedValue < BREAK){
+        speedValue = BREAK;
+    }
+    if(right >= 120){
+        steerValue = 120;
+        right = 120;
+    }
+    if(left <= 0){
+      steerValue = 0;
+      left = 0;
+    }
+     
+     SteerServo.write(steerValue);
+     EscServo.writeMicroseconds(speedValue);
+    delay(10);
+   
 }
-
-
