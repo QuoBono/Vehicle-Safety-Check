7cc3e4c60cfaf4f1f285a129775c28c2c44602ca
Ninosauleo <ninosauleo@gmail.com>
2017-03-14 12:58:44 +0100

    Added comments and clean the code

--- VehicleSafetyCheck.c
+++ VehicleSafetyCheck.c
@@ -1,49 +1,78 @@
 /*
  *author Group9
+ *
  */
+
+
+/*
+ * Speed Max and Min Values
+ */
+// Max over 1850
+// Min around 900
+//Neutral 1340 to 1380 ~= 1350
 
 #define NEUTRAL 1350
 #define FULL 1850
 #define BREAK 850
 
 #include <Servo.h>
+/*
+ * Steering Max and Min Values
+ */
 //max right 120
 //max left 0
 //straight 70
 
+
+/*
+ * Car Set-Up
+ */
 int straight = 70;
-const int speedCarInput = 5; //speed
-const int steerCarInput = 6; //steering
-const int steerCarOutput = 9 ;
-const int SpeedCarOutput = 11;
-unsigned int softwareSpeed = 1450;
+const int speedCarInput = 5; //speed input
+const int steerCarInput = 6; //steering input
+const int steerCarOutput = 9 ;//steering output
+const int SpeedCarOutput = 11; //speed output
+
+/*
+ * Remote Control
+ */
 bool joystick = false;
+
+/*
+ * Serial Control
+ */
 char userInput;
 char lastPressed;
 int neutral = NEUTRAL;
 int right = straight + 10;
 int left = straight - 10;
 
-Servo EscServo;
-Servo SteerServo;
+/*
+ * Speed and Steer Servo
+ */
+Servo EscServo; //Speed
+Servo SteerServo; //Steer
 
 void setup(){
     Serial.begin(9600);
-    
     pinMode(speedCarInput, INPUT);
     pinMode(steerCarInput, INPUT); //steering
     pinMode(steerCarOutput, OUTPUT);
     pinMode(SpeedCarOutput, OUTPUT);
-    EscServo.attach(11); //11 speed pin
-    //EscServo.attach(feedbackPin);
-    SteerServo.attach(9); //steering
+    EscServo.attach(SpeedCarOutput); //speed pin
+    SteerServo.attach(steerCarOutput); //steer pin
+    /*
+     * Set car wheels to the front and NEUTRAL speed.
+     */
     SteerServo.write(straight);
     EscServo.write(NEUTRAL);
     
 }
 
 void loop(){
-    
+    /*
+     * If the car receives a signal from the Remote control (either speed or steer) Joystick will be true else false.
+     */
     if(pulseIn(speedCarInput, HIGH, 27000) > 800 && pulseIn(steerCarInput, HIGH, 27000) > 800){
         joystick = true;
     }else{
@@ -52,32 +81,39 @@ void loop(){
     
     
     if(joystick) {
-        //SteerServo.write(100);
-        int i = pulseIn(speedCarInput, HIGH); //speed
-        int j = pulseIn(steerCarInput, HIGH); //steering
+        int i = pulseIn(speedCarInput, HIGH); //Get speed
+        int j = pulseIn(steerCarInput, HIGH); //Get steering
         
         Serial.println("this is speed");
         Serial.println(i);
         Serial.println("this is steer");
         Serial.println(j);
         
-        // Max over 1800
-        // Min around 900
-        //Neutral 1340 to 1380
-        
-        SteerServo.write(j);
         EscServo.writeMicroseconds(i); //speed
+        SteerServo.write(j); //steering
         
     } else if (!joystick || Serial.available() > 0) {
+        
+        /*
+         * Get Input from Serial
+         */
         userInput = Serial.read();
+        
+        /*
+         * Switch case for moving the car
+         */
+        
         switch(userInput){
+                /*
+                 * Last key pressed is used in order to ovoid damaging the motors when someone goes from front to backwards or the other way.
+                 */
             case 'w': if(lastPressed == 'x'){ EscServo.write(neutral); delay(1000);}EscServo.write(neutral + 100); if(neutral < 1550){neutral = neutral + 10;} lastPressed = 'w';
                 break;
             case 's': EscServo.write(NEUTRAL); SteerServo.write(straight); lastPressed = 's';
                 break;
-            case 'd': SteerServo.write(right); if(right <= 120){ right = right +10;} lastPressed = 'd';
+            case 'd': SteerServo.write(right); if(right <= 120){ right = right +10;} lastPressed = 'd'; //increment the steering if you press more.
                 break;
-            case 'a': SteerServo.write(left); if(left > 0){ left = left - 10;} lastPressed = 'a';
+            case 'a': SteerServo.write(left); if(left > 0){ left = left - 10;} lastPressed = 'a'; //increment the steering if you press more.
                 break;
             case 'x': if(lastPressed == 'w'){ EscServo.write(neutral); delay(1000);} EscServo.write(NEUTRAL - 200); SteerServo.write(straight); lastPressed = 'w';
                 break;
