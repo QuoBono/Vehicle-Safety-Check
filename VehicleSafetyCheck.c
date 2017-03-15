0e2f43032d0fe1869ccbbc6acb973a693e4d796b
Ninosauleo <ninosauleo@gmail.com>
2017-03-15 10:18:19 +0100

    Second controller

--- VehicleSafetyCheck.c
+++ VehicleSafetyCheck.c
@@ -1,7 +1,7 @@
 
 /*author Group9
-  */
- 
+ */
+
 #define NEUTRAL 1400
 #define FULL 1600
 #define BREAK 1000
@@ -10,7 +10,7 @@
 #define LEFT 60
 
 #include <Servo.h>
- 
+
 int speedCarInput = 5; //speed
 int steerCarInput = 6; //steering
 int steerCarOutput = 9 ;
@@ -25,80 +25,104 @@ int left = LEFT;
 int speedValueRev = NEUTRALREVERSE;
 bool controller = false;
 int steerValue = straight;
- 
- Servo EscServo;
- Servo SteerServo;
+
+Servo EscServo;
+Servo SteerServo;
+
 void setup(){
- 
     Serial.begin(9600);
-    EscServo.attach(11); //11 speed pin
-    SteerServo.attach(9); //steering
+    
+    pinMode(speedCarInput, INPUT);
+    pinMode(steerCarInput, INPUT); //steering
+    pinMode(steerCarOutput, OUTPUT);
+    pinMode(SpeedCarOutput, OUTPUT);
+    
+    EscServo.attach(SpeedCarOutput); //11 speed pin
+    SteerServo.attach(steerCarOutput); //steering
     SteerServo.write(straight);
     EscServo.writeMicroseconds(NEUTRAL);
- }
- 
- void loop(){
-     
-    int i = pulseIn(speedCarInput, HIGH); //speed
-    int j = pulseIn(steerCarInput, HIGH); //steering
-   
+}
+
+void loop(){
+    /*
+     int i = pulseIn(speedCarInput, HIGH); //speed
+     int j = pulseIn(steerCarInput, HIGH); //steering
+     */
+    
     if(pulseIn(speedCarInput, HIGH, 27000) > 800 && pulseIn(steerCarInput, HIGH, 27000) > 800){
         joystick = true;
     }else{
         joystick = false;
-   }
-   
-    if(joystick) {
-       Serial.println("joystick");  
-     
-
-      if(!controller){
-        
-        SteerServo.write(straight);
-        EscServo.writeMicroseconds(NEUTRAL);
-        controller = true;
-        delay(20);
-      }
+    }
     
-        int accelerate = pulseIn(speedCarInput, HIGH); //speed
-        int steer = pulseIn(steerCarInput, HIGH); //steering
+    if(joystick) {
+        //Serial.println(controller);
         
-        SteerServo.write(steer);
-        EscServo.writeMicroseconds(accelerate);
-
+        if(!controller){
+            SteerServo.write(straight);
+            EscServo.writeMicroseconds(NEUTRAL);
+            controller = true;
+            delay(2000);
+            Serial.println("remotel");
+            Serial.print(controller);
+            /*
+             int accelerate = pulseIn(speedCarInput, HIGH); //speed
+             int steer = pulseIn(steerCarInput, HIGH); //steering
+             */
+        }
+        
+        /*
+         SteerServo.write(steer);
+         EscServo.writeMicroseconds(accelerate);
+         */
+        
+        
+        
     } else if (!joystick) {
-      controller = false;
-      userInput = Serial.read();
-
+        
+        if(controller){
+            SteerServo.write(straight);
+            EscServo.writeMicroseconds(NEUTRAL);
+            controller = false;
+            delay(2000);
+            Serial.println("serial");
+            Serial.print(controller);
+            /*
+             int accelerate = pulseIn(speedCarInput, HIGH); //speed
+             int steer = pulseIn(steerCarInput, HIGH); //steering
+             */
+        }
+        userInput = Serial.read();
+        
         switch(userInput){
-            case 'w': 
-                  speedValue = speedValue+50;
-                  break;
-                  
-            case 's': 
-                  speedValue = NEUTRAL;
-                  steerValue = straight;
-                  right = RIGHT;
-                  left = LEFT;
-                  speedValueRev = NEUTRALREVERSE;
-                  break;
-                  
+            case 'w':
+                speedValue = speedValue+50;
+                break;
+                
+            case 's':
+                speedValue = NEUTRAL;
+                steerValue = straight;
+                right = RIGHT;
+                left = LEFT;
+                speedValueRev = NEUTRALREVERSE;
+                break;
+                
             case 'd':
-                  right = right + 10;
-                  steerValue = right;
-                  break;
-                  
-            case 'a':  
-                  left  = left - 10;
-                  steerValue = left;
-                  break;
-                  
-            case 'x': 
-                  EscServo.writeMicroseconds(NEUTRAL);
-                  delay(100);
-                  speedValueRev = speedValueRev - 50;
-                  speedValue = speedValueRev;
-                  break;
+                right = right + 10;
+                steerValue = right;
+                break;
+                
+            case 'a':
+                left  = left - 10;
+                steerValue = left;
+                break;
+                
+            case 'x':
+                EscServo.writeMicroseconds(NEUTRAL);
+                delay(100);
+                speedValueRev = speedValueRev - 50;
+                speedValue = speedValueRev;
+                break;
         }
     }
     
@@ -113,12 +137,12 @@ void setup(){
         right = 120;
     }
     if(left <= 0){
-      steerValue = 0;
-      left = 0;
+        steerValue = 0;
+        left = 0;
     }
-     
-     SteerServo.write(steerValue);
-     EscServo.writeMicroseconds(speedValue);
+    
+    SteerServo.write(steerValue);
+    EscServo.writeMicroseconds(speedValue);
     delay(10);
-   
+    
 }
